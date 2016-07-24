dc6f6bd90d6f85f42ea6cd44e6344165cda61885
code-for-coffee <jtraverjr@gmail.com>
2016-07-24 17:34:52 -0500

    das code

--- app/src/main/java/org/codeforcoffee/mudkiprecommendations/MapsActivity.java
+++ app/src/main/java/org/codeforcoffee/mudkiprecommendations/MapsActivity.java
@@ -2,12 +2,14 @@ package org.codeforcoffee.mudkiprecommendations;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -39,8 +41,23 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng chicago = new LatLng(41.8781, -87.6298);
+        mMap.addMarker(new MarkerOptions()
+                .position(chicago)
+                .title("Marker in Chicago")
+                .snippet("Home sweet home"));
+        // click listener for markers
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                Toast.makeText(getApplicationContext(),
+                        marker.getTitle() + " : " + marker.getSnippet(),
+                        Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
+        // move the camera
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(10));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(chicago));
     }
 }
